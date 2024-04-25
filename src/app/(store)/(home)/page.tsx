1d646022863c624d34e2d1c88a5a1ba1cc7366cd
Hikaro Eduardo <hikaroeduardobandeira@gmail.com>
2024-04-25 19:18:12 -0300

    Aplicacao utilizando as funcionalidades mais atuais de Next.js (App Router)

---
+++ src/app/(store)/(home)/page.tsx
@@ -0,0 +1,89 @@
+import Link from "next/link";
+import Image from "next/image";
+import { api } from "@/data/api";
+import { Product } from "@/data/types/product";
+import { Metadata } from "next";
+
+async function getFeaturedProducts(): Promise<Product[]> {
+    const response = await api("/products/featured", {
+        cache: "force-cache",
+        next: { revalidate: 60 * 60 },
+    });
+
+    const products = await response.json();
+
+    return products;
+}
+
+export const metadata: Metadata = {
+    title: "Home",
+};
+
+export default async function Home() {
+    const [highLightedProduct, ...otherProducts] = await getFeaturedProducts();
+
+    return (
+        <div className="grid max-h-[600px] grid-cols-9 grid-rows-6 gap-6">
+            <Link
+                href={`/product/${highLightedProduct.slug}`}
+                className="group col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end relative"
+            >
+                <Image
+                    src={highLightedProduct.image}
+                    className="my-auto group-hover:scale-105 transition-transform duration-500"
+                    width={650}
+                    height={600}
+                    quality={100}
+                    alt=""
+                />
+
+                <div className="absolute bottom-28 right-28 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+                    <span className="text-sm truncate">
+                        {highLightedProduct.title}
+                    </span>
+                    <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+                        {highLightedProduct.price.toLocaleString("pt-BR", {
+                            style: "currency",
+                            currency: "BRL",
+                            minimumFractionDigits: 0,
+                            maximumFractionDigits: 0,
+                        })}
+                    </span>
+                </div>
+            </Link>
+
+            {otherProducts.map((product) => {
+                return (
+                    <Link
+                        key={product.id}
+                        href={`/product/${product.slug}`}
+                        className="relative group col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
+                    >
+                        <Image
+                            src={product.image}
+                            className="my-auto group-hover:scale-105 transition-transform duration-500"
+                            width={650}
+                            height={600}
+                            quality={100}
+                            alt=""
+                        />
+
+                        <div className="absolute bottom-10 right-28 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+                            <span className="text-sm truncate">
+                                {product.title}
+                            </span>
+                            <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+                                {product.price.toLocaleString("pt-BR", {
+                                    style: "currency",
+                                    currency: "BRL",
+                                    minimumFractionDigits: 0,
+                                    maximumFractionDigits: 0,
+                                })}
+                            </span>
+                        </div>
+                    </Link>
+                );
+            })}
+        </div>
+    );
+}
---
+++ src/app/api/products/search/route.ts
@@ -0,0 +1,17 @@
+import data from "../data.json";
+import { z } from "zod";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    const { searchParams } = request.nextUrl;
+
+    const query = z.string().parse(searchParams.get("q"));
+
+    const products = data.products.filter((product) =>
+        product.title.toLocaleLowerCase().includes(query.toLocaleLowerCase())
+    );
+
+    return Response.json(products);
+}
